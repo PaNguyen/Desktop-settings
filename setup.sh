73f220a151bc060343409c5e0f30f2ba4c74a8f1
Patrick <pahdnguyen@gmail.com>
2015-11-14 14:25:50 -0500

    banshee codec plugins and mpv

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ apt-get install texlive-full
 apt-get install scrot
 apt-get install compizconfig-settings-manager
 apt-get install compiz-plugins
+apt-get install banshee
+apt-get install gstreamer0.10-plugins
+apt-get install mpv
